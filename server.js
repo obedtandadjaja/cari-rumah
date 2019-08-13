d0ffb1245714fcf85dabc8c0d3ce2a2d2c26c8c1
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-08-12 23:07:02 -0700

    remove unnecessary middleware

--- server.js
+++ server.js
@@ -12,17 +12,6 @@ const schema = buildSchema(typeDefs)
 // Create an express server and a GraphQL endpoint
 const app = express()
 app.use(morgan('combined', { stream: logger.stream }))
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'dev' ? err : {};
-
-  // add this line to include winston logging
-  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-
-  // render the error page
-  res.status(err.status || 500);
-})
 app.use(
   `/graphql`,
   expressGraphql({
