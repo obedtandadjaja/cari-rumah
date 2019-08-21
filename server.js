589eff1b2667d87f0a27503a2b28029a8f692ec0
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-08-21 01:23:10 -0700

    added cors middleware implementation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import logger from './src/logger'
 import graphQLHandler from './src/handlers/graphql'
 import googleHandler from './src/handlers/google'
@@ -14,9 +15,20 @@ app.use(
   [responseTimeMiddleware, authenticationMiddleware],
   graphQLHandler
 )
+
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 app.use(
   '/google/:name',
-  [responseTimeMiddleware, authenticationMiddleware],
+  [cors(corsOptions), responseTimeMiddleware, authenticationMiddleware],
   googleHandler
 )
 app.listen(4000, () => logger.info(`Express GraphQL Server Now Running On localhost:4000/graphql`))
