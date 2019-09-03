e953145ecf24659225888daafd4cfbbcb0e4f6c9
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-09-02 21:49:10 -0700

    added cors to graphql

--- server.js
+++ server.js
@@ -7,15 +7,6 @@ import googleHandler from './src/handlers/google'
 import responseTimeMiddleware from './src/middlewares/response-time'
 import authenticationMiddleware from './src/middlewares/authentication'
 
-// Create an express server and a GraphQL endpoint
-const app = express()
-app.use(morgan('combined', { stream: logger.stream }))
-app.use(
-  '/graphql',
-  [responseTimeMiddleware, authenticationMiddleware],
-  graphQLHandler
-)
-
 const whitelist = ['http://localhost:3000']
 const corsOptions = {
   origin: (origin, callback) => {
@@ -26,9 +17,22 @@ const corsOptions = {
     }
   }
 }
+
+// Create an express server and a GraphQL endpoint
+const app = express()
+app.use(morgan('combined', { stream: logger.stream }))
+app.use(
+  '/graphql',
+  [cors(corsOptions), responseTimeMiddleware, authenticationMiddleware],
+  graphQLHandler
+)
+
+// Google endpoint
 app.use(
   '/google/:name',
   [cors(corsOptions), responseTimeMiddleware, authenticationMiddleware],
   googleHandler
 )
-app.listen(4000, () => logger.info(`Express GraphQL Server Now Running On localhost:4000/graphql`))
+
+// Register app to listen on port 4000
+app.listen(4000, () => logger.info(`Express Server Now Running On localhost:4000/graphql`))
