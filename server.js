b056ec03c636c0cb575550894c055e7d59a7e125
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-09-10 23:43:15 -0700

    allow all cors on dev

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import authenticationMiddleware from './src/middlewares/authentication'
 const whitelist = ['http://localhost:3000']
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (process.env.ENV === 'dev' || whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
