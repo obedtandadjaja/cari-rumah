8b307cc37ab36b15ed1a5832bcaf9bdb13c220ae
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-09-12 21:03:06 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
-require('babel-core/register');
-require('babel-polyfill');
+require('babel-core/register')
+require('babel-polyfill')
 
 // Import the rest of our application.
 module.exports = require('./server')
